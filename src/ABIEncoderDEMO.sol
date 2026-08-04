2ba4883d212452b7460008a4ab618f54a1d58acc
Alexander Van strahlen <75139319+VanstrahlenAlex@users.noreply.github.com>
2026-08-04 15:54:37 -0500

    first commit

---
+++ src/ABIEncoderDEMO.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.36;
+
+
+/**
+ * @title ABIEncoderDEMO
+ * @author Alexander Van strahlen
+ * @notice This smart contract shows different uses of abi.encodePacked in DeFi protocols 
+ */
+
+contract ABIEncoderDEMO {
+
+}
